e1da0e5470be7b242710fb78339b2e01760a76e2
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2021-03-14 14:20:48 -0700

    Issue#1 tournament model (#9)
    * added tournaments model
    
    * spelling error

---
+++ GolfTournament/Models/Tournaments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Tournaments
+    {
+        [Key]
+        public int TournamentId { get; set; }
+        /// <summary>
+        /// Id of the course being played
+        /// </summary>
+        //public Courses Course { get; set; }
+        /////// <summary>
+        /////// Date the tournament is being played 
+        /////// </summary>
+        public DateTime ScheduledDate { get; set; }
+        /// <summary>
+        /// flights are the number of leaderboards the main tournament will be broken down into
+        /// </summary>
+        public int Flights { get; set; }
+    }
+}

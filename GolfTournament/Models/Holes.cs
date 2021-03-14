fbe6aeaada1122d21e92bdc3517bfb083d5de57c
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2021-03-14 14:28:07 -0700

    added holes model (#11)

---
+++ GolfTournament/Models/Holes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Holes
+    {
+        [Key]
+        public int HoleId { get; set; }
+        /// <summary>
+        /// number of hole on the golf course
+        /// </summary>
+        public int Number { get; set; }
+        public int Par { get; set; }
+        /// <summary>
+        /// Difficulty of hole on the course. hardest = 1, easiest = 18
+        /// </summary>
+        public int Handicap { get; set; }
+
+        public Courses Course { get; set; }
+    }
+}

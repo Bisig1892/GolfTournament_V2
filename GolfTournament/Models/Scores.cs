39c5e23bb4205fd65a292ac47380e48725230d93
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2021-03-14 14:45:44 -0700

    added scores model (#16)

---
+++ GolfTournament/Models/Scores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Scores
+    {
+        [Key]
+        public int ScoreId { get; set; }
+        [Required]
+        public int Score { get; set; }
+        /// <summary>
+        /// Team the score is for on the paticular hole
+        /// </summary>
+        [Required]
+        public Teams Teams { get; set; }
+        /// <summary>
+        /// Hole in which the score was made
+        /// </summary>
+        [Required]
+        public Holes Hole { get; set; }
+    }
+}

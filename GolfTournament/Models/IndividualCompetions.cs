428d6bf805a8b9bb276ed0f85e0d0cad5607f651
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2021-03-14 14:59:51 -0700

    added individualCompetitions model (#17)

---
+++ GolfTournament/Models/IndividualCompetions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class IndividualCompetions
+    {
+        /// <summary>
+        /// This is for hole prizes and competitions that are part of the tournament but are not based on the teams overall score. (Longest drive, chipping comp, closest to pin on second shot. etc.)
+        /// </summary>
+        [Key]
+        public int IndividualCompetionsId { get; set; }
+        /// <summary>
+        /// First name of the winner of the competion
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name of the winner of the competion
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// The description of the competition and where it took place.
+        /// Example: 1st hole - Longest drive(seniors)
+        /// Example: Practice chipping green - chipping competion
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Amount player won for competition
+        /// </summary>
+        [DataType(DataType.Currency)]
+        public int PrizeMoney { get; set; }
+    }
+}

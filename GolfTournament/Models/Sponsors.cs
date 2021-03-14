d82e1b0b2e0df9eb4abc6509a9d2bee0ab003231
Tyler <bisig1892@gmail.com>
2021-03-14 15:03:57 -0700

    added sponsors model

---
+++ GolfTournament/Models/Sponsors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Sponsors
+    {
+        [Key]
+        public int SponsorsId { get; set; }
+        /// <summary>
+        /// Name of person or business who sponsored the event.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+        /// <summary>
+        /// Tournament that is being sponsored
+        /// </summary>
+        [Required]
+        public Tournaments Tournament { get; set; }
+    }
+}

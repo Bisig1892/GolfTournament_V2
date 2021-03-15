28355b87f25d01766d367f7d9521f4269ea81ac9
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2021-03-14 19:18:36 -0700

    Added all models to database (#20)

---
+++ GolfTournament/Data/TournamentContext.cs
@@ -0,0 +1,25 @@
+﻿using GolfTournament.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolfTournament.Data
+{
+    public class TournamentContext : IdentityDbContext
+    {
+        public TournamentContext(DbContextOptions<TournamentContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<Holes> Holes { get; set; }
+        public DbSet<IndividualCompetions> IndividualCompetions { get; set; }
+        public DbSet<Players> Players { get; set; }
+        public DbSet<Scores> Scores { get; set; }
+        public DbSet<Sponsors> Sponsors { get; set; }
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<Tournaments> Tournaments { get; set; }
+    }
+}
